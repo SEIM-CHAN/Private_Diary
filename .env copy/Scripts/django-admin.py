c709e60b3bef53e7e5ed0f3cd914ea58226ecd64
SEIM <iramsekelli@gmail.com>
2021-09-13 11:36:08 +0900

    env copy deleted

--- .env copy/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\student\documents\private_diary\private_diary\.env\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
