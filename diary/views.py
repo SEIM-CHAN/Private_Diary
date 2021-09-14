ab89ca7b0de2ad1e6bc53d911b9b60c948b44417
SEIM <iramsekelli@gmail.com>
2021-09-14 12:35:38 +0900

    diary CRUD

--- diary/views.py
+++ diary/views.py
@@ -1,20 +1,22 @@
-from django.http.response import HttpResponse
-from django.shortcuts import render
 import logging
+from django.core.mail import message
+
 from django.urls import reverse_lazy
 from django.views import generic
-from .forms import InquiryForm
 from django.contrib import messages
 
+from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+from .forms import InquiryForm, DiaryCreateForm
+from .models import Diary
 
 logger = logging.getLogger(__name__)
 
-# Create your views here.
-def index(request):
-    return render(request, 'diary/index.html')
 
 class IndexView(generic.TemplateView):
-    template_name="diary/index.html"
+    template_name = "diary/index.html"
+
 
 class InquiryView(generic.FormView):
     template_name = "diary/inquiry.html"
@@ -23,25 +25,60 @@ class InquiryView(generic.FormView):
 
     def form_valid(self, form):
         form.send_email()
-        messages.success(self.request,'メッセージを送信しました')
-        logger.info('Inquiry sent by {}'.format(form.cleaned_data['name']))
+        messages.success(self.request, 'メッセージを送信しました。')
+        logger.info('inquiry send by {}'.format(form.cleaned_data['name']))
         return super().form_valid(form)
 
-from django.contrib.auth.mixins import LoginRequiredMixin
-from .models import Diary
+
 class DiaryListView(LoginRequiredMixin, generic.ListView):
     model = Diary
-    template_name = 'diary_list.html'
-
-    def get_queryset(self):
-        diaries = Diary.objects.filter(user=self.request.user).order_by('-created_at')
-        return diaries
-
-class DiaryListView(LoginRequiredMixin, generic.ListView):
-    model = Diary 
     template_name = 'diary_list.html'
     paginate_by = 2
 
     def get_queryset(self):
-        diaries = Diary.objects.filter(user=self.request.user).order_by('-created_at')
+        diaries = Diary.objects.filter(
+            user=self.request.user).order_by('-created_at')
         return diaries
+
+class DiaryDetailView(LoginRequiredMixin, generic.DetailView):
+    model = Diary
+    template_name = 'diary_detail.html'
+
+class DiaryCreateView(LoginRequiredMixin, generic.CreateView):
+    model = Diary
+    template_name = 'diary_create.html'
+    form_class = DiaryCreateForm
+    success_url = reverse_lazy('diary:diary_list')
+    
+    def form_valid(self, form):
+        diary = form.save(commit=False)
+        diary.user = self.request.user
+        diary.save()
+        messages.success(self.request, '日記を作成しました。')
+        return super().form_valid(form)
+
+    def form_invalid(self, form):
+        messages.error(self.request, "日記の作成に失敗しました")
+        return super().form_invalid(form)
+
+class DiaryUpdateView(LoginRequiredMixin, generic.UpdateView):
+    model = Diary
+    template_name = 'diary_update.html'
+    form_class = DiaryCreateForm
+
+    def get_success_url(self):
+        return reverse_lazy('diary:diary_detail', kwargs={'pk': self.kwargs['pk']})
+    def form_valid(self, form):
+        messages.success(self.request, '日記を更新しました。')
+        return super().form_valid(form)
+    def form_invalid(self, form):
+        messages.error(self.request, "日記の更新に失敗しました。")
+        return super().form_invalid(form)
+
+class DiaryDeleteView(LoginRequiredMixin, generic.DeleteView):
+    model = Diary
+    template_name = 'diary_delete.html'
+    success_url = reverse_lazy('diary:diary_list')
+    def delete(self, request, *args, **kwargs):
+        messages.success(self.request, '日記を削除しました。')
+        return super().delete(request, *args, **kwargs)
